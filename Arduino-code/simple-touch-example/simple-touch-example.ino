41b15bfd59d1f840e11de6f75e217331f4a10d63
Becky Stewart <r.stewart@imperial.ac.uk>
2022-05-23 18:08:37 +0100

    Brain dump of functionality, but basics all working

--- Arduino-code/simple-touch-example/simple-touch-example.ino
+++ Arduino-code/simple-touch-example/simple-touch-example.ino
@@ -8,7 +8,13 @@
 
 
 Trill trillSensor;
-int sensorValues[30];
+
+// arrays holding information about each touch sensing channel
+int sensorValues[30]; // raw values from the touch sensors
+int touchThresholds[30]; // threshold for what counts as a touch
+bool sensorTouches[30]; // true if currently above touch threshold
+bool prevSensorTouches[30]; // previous frame of touches
+unsigned long touchTimers[30]; // when a pin is first touched
 
 void setup() {
   // Initialise serial and touch sensor
@@ -30,9 +36,13 @@ void setup() {
   // Make sure Trill Craft is set to differential mode
   trillSensor.setMode(Trill::DIFF);
 
-  // Fill the array holding the sensor values with 0s
+  // Fill the arrays with starting data
   for (int i = 0; i < 30; i++) {
     sensorValues[i] = 0;
+    touchThresholds[i] = 400;
+    sensorTouches[i] = false;
+    prevSensorTouches[i] = false;
+    touchTimers[i] = 0;
   }
 
   // Initialise the HID keyboard
@@ -42,13 +52,60 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Request the latest touch data from Trill
   getSensorData();
-  for(int i=0; i<30; i++){
-    Serial.print(sensorValues[i]);
-    Serial.print(' ');
+
+
+  // Print the latest touch data from Trill
+  printSensorData();
+
+  for (int i = 0; i < 30; i++ ) {
+    if (isTouched(i)) {
+      Serial.print("touched ---- ");
+      Serial.println(i);
+    }
+    if (isTapped(i)) {
+      Serial.print("tapped ----------- ");
+      Serial.println(i);
+    }
   }
-  Serial.println("");
+
+  if ( isHeldTimer(24, 1500))
+    Serial.println("24 is held to time");
+}
+
+
+bool isTouched(int pin) {
+  return sensorTouches[pin];
+}
+
+bool isTapped(int pin) {
+  // the sensor was touched but has just been released
+  // (no longer being touched)
+  // and it was touched for less than a half second (500 ms)
+  unsigned long elapsedTime = millis() - touchTimers[pin];
+  if ( !sensorTouches[pin] && prevSensorTouches[pin] && elapsedTime < 500)
+    return true;
+  // otherwise return false
+  return false;
+}
+
+bool isDoubleTapped(int pin) {
+  // the sensor was tapped twice in quick succession
+  // if one tap has happened and less than 50ms has passed
+
+  // and a second tap occurs
+  //otherwise return false
+  return false
+}
+
+bool isHeldTimer(int pin, int minTime) {
+  // check if the pin has been actively touched for at least the minimum time in ms
+  unsigned long elapsedTime = millis() - touchTimers[pin];
+  if ( sensorTouches[pin] && elapsedTime > minTime )
+    return true;
+  // otherwise
+  return false;
 }
 
 
@@ -68,4 +125,40 @@ void getSensorData() {
       sensorChannel++;
     }
   }
+
+  // save the touches from that last data read as previous touches
+  for (int pin = 0; pin < 30; pin++) {
+    prevSensorTouches[pin] = sensorTouches[pin];
+  }
+
+  // update whether a pin has gone above the touch threshold
+  for (int pin = 0; pin < 30; pin++) {
+    if ( sensorValues[pin] > touchThresholds[pin] ) {
+      sensorTouches[pin] = true;
+    } else {
+      sensorTouches[pin] = false;
+    }
+  }
+
+
+  // save the current time for the timers
+  unsigned long currTime = millis();
+
+  // if the touch is new, save the current time
+  for (int pin = 0; pin < 30; pin++) {
+    if ( sensorTouches[pin] && !prevSensorTouches[pin]) {
+      touchTimers[pin] = currTime;
+    }
+  }
+}
+
+
+void printSensorData() {
+  // Print on a single line the current sensor values
+  for (int i = 0; i < 30; i++) {
+    Serial.print(sensorValues[i]);
+    Serial.print(' ');
+  }
+  Serial.println("");
+
 }
